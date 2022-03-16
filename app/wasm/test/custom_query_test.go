6aade7d79814d2ccd92e98054ebb5f1053e9231f
Ethan Frey <ethanfrey@users.noreply.github.com>
2022-03-16 13:09:22 +0100

    Start test setup for custom queries

---
+++ app/wasm/test/custom_query_test.go
@@ -0,0 +1,69 @@
+package wasm
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/CosmWasm/wasmd/x/wasm/keeper"
+	"github.com/CosmWasm/wasmd/x/wasm/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/osmosis-labs/osmosis/v7/app"
+)
+
+func SetupCustomApp(t *testing.T, addr sdk.AccAddress) (*app.OsmosisApp, sdk.Context) {
+	osmosis, ctx := CreateTestInput()
+	wasmKeeper := osmosis.WasmKeeper
+
+	storeReflectCode(t, ctx, osmosis, myActorAddress)
+
+	cInfo := wasmKeeper.GetCodeInfo(ctx, 1)
+	require.NotNil(t, cInfo)
+
+	// TODO: set up funds in this account
+
+	// TODO: set up some pools (pass as args? hardcode?)
+
+	return osmosis, ctx
+}
+
+func storeReflectCode(t *testing.T, ctx sdk.Context, osmosis *app.OsmosisApp, addr sdk.AccAddress) {
+	govKeeper := osmosis.GovKeeper
+	wasmCode, err := ioutil.ReadFile("../testdata/osmo_reflect.wasm")
+	require.NoError(t, err)
+
+	src := types.StoreCodeProposalFixture(func(p *types.StoreCodeProposal) {
+		p.RunAs = addr.String()
+		p.WASMByteCode = wasmCode
+	})
+
+	// when stored
+	storedProposal, err := govKeeper.SubmitProposal(ctx, src)
+	require.NoError(t, err)
+
+	// and proposal execute
+	handler := govKeeper.Router().GetRoute(storedProposal.ProposalRoute())
+	err = handler(ctx, storedProposal.GetContent())
+	require.NoError(t, err)
+}
+
+func instantiateReflectContract(t *testing.T, ctx sdk.Context, osmosis *app.OsmosisApp, funder sdk.AccAddress) sdk.AccAddress {
+	initMsgBz := []byte("{}")
+	contractKeeper := keeper.NewDefaultPermissionKeeper(osmosis.WasmKeeper)
+	codeID := uint64(1)
+	addr, _, err = contractKeeper.Instantiate(ctx, codeID, funder, funder, initMsgBz, "demo contract", nil)
+	require.NoError(t, err)
+
+	return addr
+}
+
+func TestQueryPool(t *testing.T) {
+	myActorAddress := RandomAccountAddress()
+	osmosis, ctx = SetupCustomApp(t, myActorAddress)
+
+	reflect := instantiateReflectContract(t, ctx, osmosis, myActorAddress)
+}
